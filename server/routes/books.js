018253054beef6b7927ae939213aac1d42a1c8a7
HilalBodon <hilal_bodon@hotmail.com>
2023-08-20 12:26:48 +0300

    get recommended books

--- server/routes/books.js
+++ server/routes/books.js
@@ -12,7 +12,7 @@ router.post('/',authMiddleware, async (req, res) => {
       const { name, author,genre, picture, review } = req.body;
       const userId = req.user._id;
       const user = await User.findById(userId);
-      const newBook = new Book({user: user, name, author,genre, picture, review,likes: 0 });
+      const newBook = new Book({user: user, name, author,genre, picture, review,likes: [] });
       await newBook.save();
       res.status(201).send(newBook);
     } catch (error) {
@@ -78,7 +78,7 @@ router.post('/like/:bookId', authMiddleware, async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const bookIdToLike = req.params.bookId;
-        
+
         const loggedInUser = await User.findById(loggedInUserId);
         const bookToLike = await Book.findById(bookIdToLike);
 
@@ -94,6 +94,55 @@ router.post('/like/:bookId', authMiddleware, async (req, res) => {
         await bookToLike.save();
 
         res.status(200).send({ message: "Book liked successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+// _________________________________________________________________
+
+router.post('/remove-like/:bookId', authMiddleware, async (req, res) => {
+    try {
+        const bookId = req.params.bookId;
+        const userId = req.user._id;
+
+        const bookToUpdate = await Book.findById(bookId);
+        if (!bookToUpdate) {
+            return res.status(404).send({ message: "Book not found" });
+        }
+
+        if (bookToUpdate.likes === 0) {
+            return res.status(400).send({ message: "Book has no likes to remove" });
+        }
+
+        if (!bookToUpdate.likes.includes(userId)) {
+            return res.status(400).send({ message: "User has not liked the book" });
+        }
+
+        const updatedLikes = bookToUpdate.likes.filter(like => like.toString() !== userId);
+
+        await Book.findByIdAndUpdate(bookId, {
+            likes: updatedLikes,
+            likesCount: updatedLikes.length
+                });
+
+        res.status(200).send({ message: "Like removed successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+// _______________________________________________________________
+
+router.get('/recommended', authMiddleware, async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const following = await User.findById(userId).select('following');
+
+        const recommendedBooks = await Book.find({ user: { $in: following.following } });
+
+        res.status(200).send(recommendedBooks);
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Internal Server Error" });
