a55908e53177b980bf631d50a3c08abc864f0540
HilalBodon <hilal_bodon@hotmail.com>
2023-08-20 07:52:36 +0300

    get all get my books

--- server/models/book.js
+++ server/models/book.js
@@ -6,6 +6,8 @@ const bookSchema = new mongoose.Schema({
   picture: String,
   review:  String ,
   likes: { type: Number, default: 0 },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+
 });
 
 const Book = mongoose.model("Book", bookSchema);
--- server/routes/books.js
+++ server/routes/books.js
@@ -2,22 +2,36 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth.middleware');
 const { Book } = require("../models/book");
+const { User } = require("../models/user");
 
-
-router.post('/', async (req, res) => {
+// ___________________________________________________
+router.post('/',authMiddleware, async (req, res) => {
     try {
       const { name, author, picture, review } = req.body;
-      const userId = req.user.id;
-      const newBook = new Book({ name, author, picture, review,user: userId });
+      const userId = req.user._id;
+      const user = await User.findById(userId);
+      const newBook = new Book({user: user, name, author, picture, review, });
       await newBook.save();
       res.status(201).send(newBook);
     } catch (error) {
-      console.error(error); // Log the error to the console for debugging
+      console.error(error);
       res.status(500).send({ message: "Internal Server Error" });
     }
   });
+//   _____________________________________________________
 
+  router.get('/my-books', authMiddleware, async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const userBooks = await Book.find({ user: userId });
+        res.status(200).send(userBooks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
 
+// ________________________________________________________
   router.get('/',authMiddleware, async (req, res) => {
     try {
     const books = await Book.find();
@@ -26,5 +40,8 @@ router.post('/', async (req, res) => {
     res.status(500).send({ message: "Internal Server Error" });
   }
 });
+// _______________________________________________________
+
+
 
 module.exports = router;
