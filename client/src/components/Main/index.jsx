1a81a8af3f76c388286942474302661fc3a1c6ca
HilalBodon <hilal_bodon@hotmail.com>
2023-08-21 14:23:48 +0300

    toggling thrue recommended and all books

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,23 +1,24 @@
-import BookDiscovery from "../Search/search";
-import styles from "./styles.module.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BookCard from "../BookCard";
 import AddBookForm from "../AddBookForm";
 import jwt_decode from "jwt-decode";
+import RecommendedBooks from "../RecommendedBooks";
+import BookDiscovery from "../Search/search";
+import styles from "./styles.module.css";
 
 const Main = ({ authToken }) => {
+  const [userId, setUserId] = useState(null);
+  const [showAddBookForm, setShowAddBookForm] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [showRecommended, setShowRecommended] = useState(false);
 
-	const [userId, setUserId] = useState(null);
-	const [showAddBookForm, setShowAddBookForm] = useState(false);
-	const [books, setBooks] = useState([]);
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
-	const handleLogout = () => {
-	  localStorage.removeItem("token");
-	  window.location.reload();
-	};
-
-   useEffect(() => {
+  useEffect(() => {
     const decodedToken = jwt_decode(authToken);
     const fetchedUserId = decodedToken._id;
     setUserId(fetchedUserId);
@@ -41,44 +42,56 @@ const Main = ({ authToken }) => {
     setBooks([...books, newBook]);
   };
 
-	return (
-	  <div className={styles.main_container}>
-		<nav className={styles.navbar}>
-		  <h1>ShareBook</h1>
-
-
+  return (
+    <div className={styles.main_container}>
+      <nav className={styles.navbar}>
+        <h1>ShareBook</h1>
         <button
           className={styles.btn}
           onClick={() => setShowAddBookForm(!showAddBookForm)}
         >
           + Add New Book
         </button>
-    
-		  <button className={styles.white_btn} onClick={handleLogout}>
-			Logout
-		  </button>
-		</nav>
+        <button
+          className={styles.btn}
+          onClick={() => setShowRecommended(!showRecommended)}
+        >
+          {showRecommended ? "All Books" : "Recommended Books"}
+        </button>
+        <button className={styles.white_btn} onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
 
-		<div className={styles.search_add}>
-		<BookDiscovery authToken={authToken} />
-		</div>
-		
+      <div className={styles.search_add}>
+        <BookDiscovery authToken={authToken} />
+      </div>
+
+      <div className={styles.books_container}>
+        {showRecommended ? (
+          <RecommendedBooks authToken={authToken} />
+        ) : (
+          books.map((book) => (
+            <BookCard
+              key={book._id}
+              book={book}
+              authToken={authToken}
+              userId={userId}
+            />
+          ))
+        )}
+      </div>
+
       {!showAddBookForm && (
-		<AddBookForm authToken={authToken} onBookAdded={handleBookAdded} setShowAddBookForm={setShowAddBookForm} />
+        <AddBookForm
+          authToken={authToken}
+          onBookAdded={handleBookAdded}
+          setShowAddBookForm={setShowAddBookForm}
+        />
       )}
-		<div className={styles.books_container}>
-			{books.map((book) => (
-          <BookCard
-            key={book._id}
-            book={book}
-            authToken={authToken}
-            userId={userId} 
-          />
-			))}
-		</div>
-
-	  </div>
-	);
-  };
+    </div>
+  );
+};
 
 export default Main;
+
